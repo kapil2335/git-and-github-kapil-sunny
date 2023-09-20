fccae364d1fff191ac1c83080b095d999448f0e6
kapil <kyadav4@wisc.edu>
2023-09-19 20:26:48 -0500

    5 times changes

--- bottles.py
+++ bottles.py
@@ -1,16 +1,16 @@
 beverage = "Diet Coke"
-count = 2
+count = 5
 
-print(count, "bottles of", beverage, "on the wall")
-print(count, "bottles of", "water")
-print("Take one down, pass it around")
-count = count - 1
-print(count, "bottles of", beverage, "on the wall")
+for num in range(count):
+ print(count, "bottles of", beverage, "on the wall")
+ print(count, "bottles of", "water")
+ print("Take one down, pass it around")
+ count = count - 1
+ if(count!=0):
+  print(count, "bottles of", beverage, "on the wall")
+ else:
+  print("No  more", "bottles of", beverage, "on the wall")
+ print("")
 
-print("")
 
-print(count, "bottles of", beverage, "on the wall")
-print(count, "bottles of", beverage)
-print("Take one down, pass it around")
-count = count - 1
-print("No  more", "bottles of", beverage, "on the wall")
+
